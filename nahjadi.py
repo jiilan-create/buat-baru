3b79991000f15c430d72ce33300ea3fb73f94b2f
jiilan-create <66149604+jiilan-create@users.noreply.github.com>
2020-06-04 11:42:29 +0700

    Add files via upload

---
+++ nahjadi.py
@@ -0,0 +1,12 @@
+import sys,time
+def jalan (kata):
+    for e in kata:
+       sys.stdout.write(e)
+       sys.stdout.flush()
+       time.sleep(0.1)
+jalan("halo aku tau tadi kamu pasti repot".center(44))
+jalan("tapi boleh gak aku nanya".center(44))
+jalan("mau gak kamu jadi kekasih hati ku dan mengisi hati ini dengan cintamu".center(44))
+jalan("semoga kamu mau ya jadi kekasih hatiku".center(44))
+jalan("maafkan aku bika aku seringkali membuat kesalahan".center(44))
+jalan("silahkan di jawan please".center(44))
